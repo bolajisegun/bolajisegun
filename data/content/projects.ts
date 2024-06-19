95635c4c42d810f125352b4547d18776fd4f1b02
bolajisegun <bolajisegun2015@gmail.com>
2024-06-19 10:10:46 +0100

    added new projects

--- data/content/projects.ts
+++ data/content/projects.ts
@@ -4,6 +4,23 @@ import { Project } from "types";
 const projects: Project[] = [
   {
     id: 0,
+    title: "Getlinked",
+    desc: "Just a simple landing with a stylish design",
+    img: "/static/projects/getlinked.png",
+    link: "https://getlinked-opal-ten.vercel.app/",
+    tags: ["next js", "Tailwind CSS"],
+  },
+  {
+    id: 1,
+    title: "Food delivery",
+    desc: "Just a simple food ordering web application that allow you make an order",
+    img: "/static/projects/food-delivery-app.png",
+    link: "https://bolajisegun.github.io/pagination/dist",
+    github: "https://github.com/bolajiSegun/pagination",
+    tags: ["Next js", "Prisma", "Tailwind CSS"],
+  },
+  {
+    id: 2,
     title: "Pagination Sample",
     desc: "Just a simple pagination with typescript",
     img: "/static/projects/pagination.png",
@@ -12,7 +29,7 @@ const projects: Project[] = [
     tags: ["Typescript", "CSS"],
   },
   {
-    id: 1,
+    id: 3,
     title: "Laundry Website",
     desc: "Beautiful laundry website for a dry-cleaning company",
     img: "/static/projects/zillionaire.png",
@@ -20,7 +37,7 @@ const projects: Project[] = [
     tags: ["HTML", "CSS", "Javascript"],
   },
   {
-    id: 2,
+    id: 4,
     title: "Basic Landing Page",
     desc: "Just a landing page design to practice your proficiency in web-development fundamentals",
     img: "/static/projects/balo.png",
@@ -29,9 +46,9 @@ const projects: Project[] = [
     tags: ["HTML", "CSS", "Javascript"],
   },
   {
-    id: 3,
+    id: 5,
     title: "React",
-    desc: "A fun way to check the price of bitcoin! Cloudy days, sunny days, and Bitcoin Storms!",
+    desc: "A react project that allows a user to signup and login",
     img: "/static/projects/react.png",
     link: "https://bitcointemp.com",
     github: "https://github.com/bolajiSegun/react-project",
