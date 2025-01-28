5de4b9451958fda2b8aedcefa0303e84ad55f109
Bolaji Oluwasegun <sgventures8673@gmail.com>
2025-01-28 18:57:04 +0100

    Added an update

--- data/content/projects.ts
+++ data/content/projects.ts
@@ -2,8 +2,17 @@ import { kebabCase, kebabArray } from "@/utils/utils";
 import { Project } from "types";
 
 const projects: Project[] = [
+  
   {
     id: 0,
+    title: "Animated Landing Page",
+    desc: "A website that allow you track your progress",
+    img: "/static/projects/getlinked.png",
+    link: "https://trackit-seven-phi.vercel.app/",
+    tags: ["Next JS", "Tailwind CSS", "Framer-motion", "Typescript"],
+  },
+  {
+    id: 1,
     title: "Getlinked",
     desc: "A simple eCommerce website that allows you to upload your product and display them for sale",
     img: "/static/projects/getlinked.png",
@@ -12,7 +21,7 @@ const projects: Project[] = [
     tags: ["Next JS", "Tailwind CSS", "Framer-motion"],
   },
   {
-    id: 1,
+    id: 2,
     title: "Blog Website",
     desc: "A simple blog website where you can create an account and add a blog post and view other blogs from other users",
     img: "/static/projects/blog.png",
@@ -21,7 +30,7 @@ const projects: Project[] = [
     tags: ["React", "Typescript", "Tailwind Css", "Node js"],
   },
   {
-    id: 2,
+    id: 3,
     title: "React",
     desc: "Just a simple react website that encompasses the use of authentication and authorization functionality, no special concentration the ui.",
     img: "/static/projects/react.png",
@@ -30,7 +39,7 @@ const projects: Project[] = [
     tags: ["React", "Tailwind", "API", "Node.js"],
   },
   {
-    id: 3,
+    id: 4,
     title: "Laundry Website",
     desc: "Beautiful laundry website for a dry-cleaning company",
     img: "/static/projects/zillionaire.png",
@@ -38,7 +47,7 @@ const projects: Project[] = [
     tags: ["HTML", "CSS", "Javascript"],
   },
   {
-    id: 4,
+    id: 5,
     title: "Ecommerce Website",
     desc: "Beautiful Ecommerce website with full payment gateway integration for african bespoke wears",
     img: "/static/projects/kenzcouture.png",
@@ -46,7 +55,7 @@ const projects: Project[] = [
     tags: ["Wordpress"],
   },
   {
-    id: 5,
+    id: 6,
     title: "Basic Landing Page",
     desc: "Just a landing page design to practice your proficiency in web-development fundamentals",
     img: "/static/projects/balo.png",
@@ -55,7 +64,7 @@ const projects: Project[] = [
     tags: ["HTML", "CSS", "Javascript"],
   },
   {
-    id: 6,
+    id: 7,
     title: "eCommerce Website",
     desc: "A simple eCommerce website that allows you to upload your product and display them for sale",
     img: "/static/projects/sg-store.png",
